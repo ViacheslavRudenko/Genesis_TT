26e98c1750cff3eda8e557c75c0de63b28a34e6b
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-16 11:51:26 +0200

    feat: Add Container component and apply custom styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Container } from "@mui/material";
 import { FC } from "react";
 
 const App: FC = () => {
-  return <>asd</>;
+  return <Container maxWidth="lg" sx={styles.appContainer}></Container>;
+};
+
+const styles = {
+  appContainer: {
+    display: "flex",
+    justifyContent: "center",
+    minHeight: "100vh",
+    minWidth: "320px",
+  },
 };
 
 export default App;
