abb703c0139acee468d09ba5b33b748f939ced4c
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-16 21:55:54 +0200

    fix: bug with fetch data

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
-const corsAnywhereUrl: string = 'https://cors-anywhere.herokuapp.com/';
 const url: string = process.env.REACT_APP_API_COURSES ?? "";
 
 const config: AxiosInstance = axios.create({
-    baseURL: corsAnywhereUrl + url,
+    baseURL: url,
+
 });
 
 export default config;
