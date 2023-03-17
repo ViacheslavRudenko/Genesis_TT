430e4e343060e5205f0390dbe6980e0dc0deed6d
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-17 19:47:07 +0200

    feat: add list and item lessons

--- src/components/application/Lesson/LessonItem.tsx
+++ src/components/application/Lesson/LessonItem.tsx
@@ -1,9 +1,49 @@
+import { Grid } from "@mui/material";
 import { FC } from "react";
+import { LessonTypes } from "../../../types/course";
+import Btn from "../../ui/Btn";
+import VideoPlayer from "../../ui/VideoPlayer";
 
-const LessonsItem: FC<LessonsItemTypes> = () => {
-  return <></>;
+const LessonsItem: FC<LessonsItemTypes> = ({
+  lesson,
+  isLessonOpen,
+  changeLesson,
+}) => {
+  const { title, duration, status, previewImageLink, order } = lesson;
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={3}>
+        Lesson № {lesson.order}
+      </Grid>
+      <Grid item xs={7} sm={5}>
+        {title}
+      </Grid>
+      <Grid item display={{ xs: "none", sm: "flex" }} md={2}>
+        {duration} minutes
+      </Grid>
+      <Grid item xs={2}>
+        <Btn
+          click={() => {
+            changeLesson(lesson.order);
+          }}
+          disabled={status === "locked" ? true : false}
+        >
+          {status === "locked" ? "Locked" : "Open"}
+        </Btn>
+        {isLessonOpen && (
+          <VideoPlayer
+            videoSourceUrl={`${previewImageLink}/lesson-${order}.webp`}
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
 };
 
-type LessonsItemTypes = {};
+type LessonsItemTypes = {
+  lesson: LessonTypes;
+  isLessonOpen: boolean;
+  changeLesson: (lesson: number) => void;
+};
 
 export default LessonsItem;
--- src/components/application/Lesson/LessonsList.tsx
+++ src/components/application/Lesson/LessonsList.tsx
@@ -1,9 +1,31 @@
-import { FC } from "react";
+import { Grid } from "@mui/material";
+import { FC, useState } from "react";
+import { LessonTypes } from "../../../types/course";
+import LessonsItem from "./LessonItem";
 
-const LessonsList: FC<LessonsListTypes> = () => {
-  return <></>;
+const LessonsList: FC<LessonsListTypes> = ({ lessons }) => {
+  const [openLesson, setOpenLesson] = useState<number>(1);
+
+  const changeOpenLesson = (lesson: number): void => {
+    setOpenLesson(lesson);
+  };
+
+  return (
+    <Grid>
+      {lessons.map((lesson: LessonTypes) => (
+        <LessonsItem
+          lesson={lesson}
+          isLessonOpen={openLesson === lesson.order}
+          key={lesson.id}
+          changeLesson={changeOpenLesson}
+        />
+      ))}
+    </Grid>
+  );
 };
 
-type LessonsListTypes = {};
+type LessonsListTypes = {
+  lessons: LessonTypes[];
+};
 
 export default LessonsList;
