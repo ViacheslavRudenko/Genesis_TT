13e3ebb4f2fe5463b40d3c925c8c40668e09ac5a
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-18 12:47:24 +0200

    feat: add Courser detail component

---
+++ src/components/application/Courses/CourseDetail.tsx
@@ -0,0 +1,31 @@
+import { Box, Typography } from "@mui/material";
+import { FC } from "react";
+import { CourseTypes } from "../../../types/course";
+
+const CourseDetails: FC<CourseDetailsTypes> = ({ course }) => {
+  return (
+    <Box sx={styles.box}>
+      <Typography component="h6" variant="h6" align="center" fontWeight="bold">
+        {course.title}
+      </Typography>
+      <Typography align="center" paddingY={1}>
+        {course.description}
+      </Typography>
+    </Box>
+  );
+};
+
+type CourseDetailsTypes = {
+  course: CourseTypes;
+};
+
+const styles = {
+  box: {
+    border: "1px solid grey",
+    borderRadius: "20px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+    paddingY: 3,
+  },
+};
+
+export default CourseDetails;
