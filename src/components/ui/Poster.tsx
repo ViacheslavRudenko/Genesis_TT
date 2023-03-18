d50f5d5dd4484303dfc55f01fcdabddeeecf4b5f
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-18 22:32:17 +0200

    feat: add img in Poster when current img is not available

--- src/components/ui/Poster.tsx
+++ src/components/ui/Poster.tsx
@@ -2,9 +2,24 @@ import { CardMedia, Stack } from "@mui/material";
 import { FC } from "react";
 
 const Poster: FC<PosterProps> = ({ img }) => {
+  const setDefaultImg = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ): void => {
+    console.log("ok");
+
+    const img = event.target as HTMLImageElement;
+    img.src =
+      "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+  };
   return (
     <Stack sx={styles.imgBox}>
-      <CardMedia component="img" sx={styles.img} src={img} alt="poster-img" />
+      <CardMedia
+        component="img"
+        sx={styles.img}
+        src={img}
+        alt="poster-img"
+        onError={setDefaultImg}
+      />
     </Stack>
   );
 };
