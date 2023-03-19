f272d56ac69735a6b35230712782425710cc1f07
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-19 19:46:28 +0200

    feat: add that errors are automatically remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { Container } from "@mui/material";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import AlertList from "./components/application/AlertList";
 import Routing from "./components/application/Routing";
 import Context from "./context";
+import { ErrorForAlertTypes } from "./types/context";
 
 const App: FC = () => {
-  const [err, setErr] = useState<string[]>([]);
+  const [err, setErr] = useState<ErrorForAlertTypes[]>([]);
 
-  const addErr = (error: string): void => {
+  useEffect(() => {
+    // Remove the last added err from the array
+    const intervalId = setInterval(() => {
+      const updatedItems = err.slice(0, -1);
+      setErr(updatedItems);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [err]);
+
+  const addErr = (error: ErrorForAlertTypes): void => {
     setErr([error, ...err]);
   };
 
